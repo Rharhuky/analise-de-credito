aaa86185d04978d90f11629409e1fa224ce12fe6
Rharhuky <rharhuky@gmail.com>
2024-04-14 00:56:24 -0300

    add: sms logic send

--- notification-app/src/main/java/com/rharhuky/notificationapp/service/SnsService.java
+++ notification-app/src/main/java/com/rharhuky/notificationapp/service/SnsService.java
@@ -1,11 +1,20 @@
 package com.rharhuky.notificationapp.service;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.PublishRequest;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class SnsService {
 
-    public void notificar(String message){
-        // TODO
+    private final AmazonSNS amazonSNS;
+
+    public void notificar(String message, String telefone){
+        PublishRequest publishRequest = new PublishRequest()
+                .withMessage(message)
+                .withPhoneNumber(telefone);
+        amazonSNS.publish(publishRequest);
     }
 }
