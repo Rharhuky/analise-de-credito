93b821d378f2c6d8db724ecef298f1b268aee4bf
Rharhuky <rharhuky@gmail.com>
2024-04-30 08:35:32 -0300

    add: logica de analise

--- analise-credito/src/main/java/com/rharhuky/analisecredito/listener/PropostaPendenteListener.java
+++ analise-credito/src/main/java/com/rharhuky/analisecredito/listener/PropostaPendenteListener.java
@@ -1,15 +1,20 @@
 package com.rharhuky.analisecredito.listener;
 
 import com.rharhuky.analisecredito.domain.Proposta;
+import com.rharhuky.analisecredito.service.interfaces.analise.AnaliseService;
+import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Configuration;
 
+@AllArgsConstructor
 @Configuration
 public class PropostaPendenteListener {
 
+    private final AnaliseService analiseService;
+
     @RabbitListener(queues = {"${rabbitmq.queue.proposta-pendente}"})
     public void propostaEmAnalise(Proposta proposta){
-
+        analiseService.calcular(proposta);
     }
 
 }
--- analise-credito/src/main/java/com/rharhuky/analisecredito/service/impl/analise/AnaliseServiceImpl.java
+++ analise-credito/src/main/java/com/rharhuky/analisecredito/service/impl/analise/AnaliseServiceImpl.java
@@ -1,37 +1,50 @@
 package com.rharhuky.analisecredito.service.impl.analise;
 
+import com.rharhuky.analisecredito.annotations.AnaliseException;
 import com.rharhuky.analisecredito.domain.Proposta;
-import com.rharhuky.analisecredito.exceptions.CalculoException;
+import com.rharhuky.analisecredito.service.NotificationRabbitMQService;
 import com.rharhuky.analisecredito.service.interfaces.analise.AnaliseService;
 import com.rharhuky.analisecredito.service.interfaces.calculo.CalculoPonto;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
-@AllArgsConstructor
 @Service
 public class AnaliseServiceImpl implements AnaliseService {
 
+    @Value(value = "${rabbitmq.exchange.proposta.concluida}")
+    private String exchangePropostaConcluida;
+
+    @Autowired
     private List<CalculoPonto> calculoPontoList;
+
+    @Autowired
+    private NotificationRabbitMQService notificationRabbitMQService;
 
     @Value(value = "${app.pontuacao.minima}")
     private int pontuacaoMinima;
 
     @Override
     public void calcular(Proposta proposta) {
-
         try{
-            var aprovada = calculoPontoList
+            var pontuacao = calculoPontoList
                     .stream()
                     .mapToInt(calculoPonto -> calculoPonto.calcular(proposta))
-                    .sum() > pontuacaoMinima;
-            proposta.setAprovado(aprovada);
+                    .sum();
+            proposta.setAprovado(pontuacao > pontuacaoMinima);
         }
-        catch (CalculoException calculoException){
+        catch (Exception exception){
+            AnaliseException analiseException = exception.getClass().getAnnotation(AnaliseException.class);
+            if(Objects.nonNull(analiseException)){
+                proposta.setObservacao(System.getenv(analiseException.value()));
+            }
             proposta.setAprovado(false);
         }
+        notificationRabbitMQService.notificar(exchangePropostaConcluida, proposta);
     }
 
 }
