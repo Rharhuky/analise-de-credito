638bf982ab547be42c523d6d68bfeda60bdc3574
Rharhuky <rharhuky@gmail.com>
2024-05-02 23:06:45 -0300

    add: websocket

--- proposta-app/src/main/java/com/rharhuky/serviceapp/listener/PropostaConcluidaListener.java
+++ proposta-app/src/main/java/com/rharhuky/serviceapp/listener/PropostaConcluidaListener.java
@@ -1,7 +1,9 @@
 package com.rharhuky.serviceapp.listener;
 
 import com.rharhuky.serviceapp.entity.Proposta;
+import com.rharhuky.serviceapp.mapper.PropostaMapper;
 import com.rharhuky.serviceapp.repository.PropostaRepository;
+import com.rharhuky.serviceapp.service.WebSocketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -12,9 +14,12 @@ public class PropostaConcluidaListener {
 
     private final PropostaRepository propostaRepository;
 
+    private final WebSocketService webSocketService;
+
     @RabbitListener(queues = "${rabbitmq.queue.proposta-concluida}")
     public void concluirProposta(Proposta proposta){
         propostaRepository.save(proposta);
+        webSocketService.notificar(PropostaMapper.CONVERT.convertToPropostaResponseDto(proposta));
     }
 
 }
---
+++ proposta-app/src/main/java/com/rharhuky/serviceapp/service/WebSocketService.java
@@ -0,0 +1,21 @@
+package com.rharhuky.serviceapp.service;
+
+import com.rharhuky.serviceapp.dto.PropostaResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class WebSocketService {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+
+    public void notificar(PropostaResponseDto propostaResponseDto){
+        simpMessagingTemplate.convertAndSend("/propostas", propostaResponseDto);
+
+
+    }
+
+}
