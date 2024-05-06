b73f0e9e6b79cd090ac29bffb5c3ae3cda458b62
Rharhuky <rharhuky@gmail.com>
2024-05-06 12:56:05 -0300

    refact: correcao WebSocketConfig

--- proposta-app/src/main/java/com/rharhuky/serviceapp/config/WebSocketConfig.java
+++ proposta-app/src/main/java/com/rharhuky/serviceapp/config/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/propostas");
+        registry.enableSimpleBroker("/proposta");
     }
 }
